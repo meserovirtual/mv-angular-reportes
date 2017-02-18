a01ecf8cf0315deb8721606542d5f1a8e8e9e78f
meserovirtual <meserovirtualarg@gmail.com>
2017-02-18 20:29:45 -0200

    Mateo
    Actualización de deudores

--- mv-deudores.js
+++ mv-deudores.js
@@ -14,31 +14,61 @@
         }
     }
 
-    mvDeudoresController.$inject = ["$rootScope", "$location", 'UserService', 'UserVars', 'SucursalesService',
-        'MvUtilsGlobals', 'MvUtils'];
-    function mvDeudoresController($rootScope, $location, UserService, UserVars, SucursalesService,
-                                  MvUtilsGlobals, MvUtils) {
+    mvDeudoresController.$inject = ["$rootScope", "$location", 'UserService', 'UserVars', 'MvUtils'];
+    function mvDeudoresController($rootScope, $location, UserService, UserVars, MvUtils) {
         var vm = this;
         vm.usuario = {};
-        vm.sucursal = {};
-        vm.sucursales = [];
         vm.usuarios = []
 
+        vm.cancel = cancel;
 
-        SucursalesService.get().then(function (data) {
-            vm.sucursales = data;
-            vm.sucursal = data[0];
-        }).catch(function(data){
-            console.log(data);
-        });
 
         UserService.getDeudores().then(function (data) {
-            console.log(data);
             vm.usuarios = data.data;
             vm.usuario = data.data[0];
         }).catch(function(data){
             console.log(data);
         });
+
+
+        function cancel() {
+            vm.usuarios = [];
+            vm.usaurio = {};
+            vm.detailsOpen=false;
+            UserVars.clearCache = true;
+        }
+
+
+        // Implementación de la paginación
+        vm.start = 0;
+        vm.limit = UserVars.paginacion;
+        vm.pagina = UserVars.pagina;
+        vm.paginas = UserVars.paginas;
+
+        function paginar(vars) {
+            if (vars == {}) {
+                return;
+            }
+            vm.start = vars.start;
+            vm.pagina = vars.pagina;
+        }
+
+        vm.next = function () {
+            paginar(MvUtils.next(UserVars));
+        };
+        vm.prev = function () {
+            paginar(MvUtils.prev(UserVars));
+        };
+        vm.first = function () {
+            paginar(MvUtils.first(UserVars));
+        };
+        vm.last = function () {
+            paginar(MvUtils.last(UserVars));
+        };
+
+        vm.goToPagina = function () {
+            paginar(MvUtils.goToPagina(vm.pagina, UserVars));
+        }
 
 
     }
